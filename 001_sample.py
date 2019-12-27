f9afde99119bb8a756bab7c0eca0cb67357afe52
Anjan Kumar <41472950+anjan111@users.noreply.github.com>
2019-12-28 02:00:30 +1300

    Add files via upload

---
+++ 001_sample.py
@@ -0,0 +1,10 @@
+#script mode / batch mode
+'''
+===>> new file
+===>> write python statement
+===>> save as .py
+===>> run it
+'''
+print "hello"
+print "hai"
+print "bye"
---
+++ 002_python basic syntax.py
@@ -0,0 +1,9 @@
+# single line comment
+'''
+--->> this is
+multiline comment
+example
+'''
+print "hello" # statement
+print "hai" # statement
+print "bye" # statement
